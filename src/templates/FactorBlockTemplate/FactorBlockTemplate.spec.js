4676641e25c780e5a307f8f2b2da0b397ef6e936
Andrew Sunada <asunada@mozilla.com>
2020-05-08 14:05:59 -0700

    adding unit tests for factorblocktemplate

--- src/templates/FactorBlockTemplate/FactorBlockTemplate.spec.js
+++ src/templates/FactorBlockTemplate/FactorBlockTemplate.spec.js
@@ -1,34 +1,77 @@
 import { mount } from '@vue/test-utils';
 import FactorBlockTemplate from './FactorBlockTemplate.vue';
 
-// Here are some Jasmine 2.0 tests, though you can
-// use any test runner / assertion library combo you prefer
 describe('FactorBlockTemplate', () => {
   it('is a Vue instance', () => {
     const wrapper = mount(FactorBlockTemplate);
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
-  // Inspect the raw component options
-  it('has a created hook', () => {
-    // expect(typeof FactorblocktemplateComponent.created).toBe('function');
+  it('receives searchBarLabel', () => {
+    const testText = 'searchBarLabel';
+    const wrapper = mount(FactorBlockTemplate, {
+      propsData: {
+        searchBarLabel: testText,
+      },
+    });
+    expect(wrapper.vm.$props.searchBarLabel).toEqual(testText);
   });
-  // Evaluate the results of functions in
-  // the raw component options
-  it('sets the correct default data', () => {
-    // expect(typeof FactorblocktemplateComponent.data).toBe('function')
-    // const defaultData = FactorblocktemplateComponent.data();
-    // expect(defaultData.message).toBe('hello!');
+
+  it('receives searchBarHandler', () => {
+    const testFunc = (value) => value;
+    const wrapper = mount(FactorBlockTemplate, {
+      propsData: {
+        searchBarHandler: testFunc,
+      },
+    });
+    expect(wrapper.vm.$props.searchBarHandler(7)).toEqual(7);
   });
-  // Inspect the component instance on mount
-  it('correctly sets the message when created', () => {
-    // const vm = new Vue(FactorblocktemplateComponent).$mount();
-    // expect(vm.message).toBe('bye!');
+
+  it('receives slot logo', () => {
+    const testSrc = `https://www.mozilla.org/media/protocol/img/logos/mozilla/black.svg`;
+    const wrapper = mount(FactorBlockTemplate, {
+      slots: {
+        logo: `<img src='${testSrc}' />`,
+      },
+    });
+    expect(wrapper.vm.$slots.logo[0].data.attrs.src).toEqual(testSrc);
   });
-  // Mount an instance and inspect the render output
-  it('renders the correct message', () => {
-    // const Ctor = Vue.extend(FactorblocktemplateComponent);
-    // const vm = new Ctor().$mount();
-    // expect(vm.$el.textContent).toBe('bye!');
+
+  it('receives slot nav', () => {
+    const wrapper = mount(FactorBlockTemplate, {
+      slots: {
+        nav: `<p>nav</p>`,
+      },
+    });
+    expect(wrapper.vm.$slots.nav[0].children[0].text).toEqual('nav');
+  });
+
+  it('receives slot profile', () => {
+    const wrapper = mount(FactorBlockTemplate, {
+      slots: {
+        profile: `<p>profile</p>`,
+      },
+    });
+    expect(wrapper.vm.$slots.profile[0].children[0].text).toEqual('profile');
+  });
+
+  it('receives slot main', () => {
+    const wrapper = mount(FactorBlockTemplate, {
+      slots: {
+        main: `<p>main</p>`,
+      },
+    });
+    expect(wrapper.vm.$slots.main[0].children[0].text).toEqual('main');
+  });
+
+  it('receives slot footerLinks', () => {
+    const wrapper = mount(FactorBlockTemplate, {
+      slots: {
+        footerLinks: `<p>footerLinks</p>`,
+      },
+    });
+    expect(wrapper.vm.$slots.footerLinks[0].children[0].text).toEqual(
+      'footerLinks',
+    );
   });
 });
 
